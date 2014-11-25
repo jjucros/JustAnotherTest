da9cfacd90247afcd2d97a08dd76649f35d2f354
Juan José Ucrós O. <jjucros@gmail.com>
2014-11-24 20:47:15 -0500

    Add a simple output change.

--- JustAnotherTest/src/appMain.java
+++ JustAnotherTest/src/appMain.java
@@ -4,6 +4,9 @@ public class appMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es solo una prueba de como se integra GitHub & Eclipse");
+		System.out.println("Ya realice mi primer cambio");
+		
+		System.out.println("Yuju!!!!");
 
 	}
 
