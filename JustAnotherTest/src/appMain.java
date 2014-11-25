acd1c5af0002f5021dfa74c8d61843e50a1c49c5
Juan José Ucrós O. <jjucros@gmail.com>
2014-11-24 20:36:29 -0500

    Intial commit of the project

---
+++ JustAnotherTest/src/appMain.java
@@ -0,0 +1,10 @@
+
+public class appMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es solo una prueba de como se integra GitHub & Eclipse");
+
+	}
+
+}
